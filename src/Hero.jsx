e124cf46784f421646fb1741d3317830b28e8d90
John Wesley L <148236413+johnwesley755@users.noreply.github.com>
2024-12-29 19:12:26 +0530

    msg

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -7,7 +7,7 @@ import resumePdf from "../src/assets/resume.pdf";
 const Hero = () => {
   return (
     <section
-      className="relative min-h-screen bg-cover bg-center text-white flex items-center overflow-hidden"
+      className="relative min-h-screen bg-cover bg-black bg-center text-white flex items-center overflow-hidden"
       style={{
         backgroundImage:
           "url('https://images.rawpixel.com/image_800/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvbHIvdjEwMTYtYy0wOF8xLWtzaDZtemEzLmpwZw.jpg')",
@@ -38,7 +38,7 @@ const Hero = () => {
         {/* Left Content */}
         <div className="flex-1 text-center md:text-left">
           <motion.h2
-            className="text-gray-300 text-lg md:text-xl tracking-widest mb-3 uppercase"
+            className="text-gray-300 text-lg md:text-xl max-md:text-xs max-sm:mt-24 tracking-widest mb-3 uppercase"
             initial={{ opacity: 0, y: -20 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 1 }}
@@ -72,7 +72,7 @@ const Hero = () => {
 
           {/* Buttons */}
           <motion.div
-            className="flex justify-center md:justify-start gap-6"
+            className="flex max-md:flex-col justify-center md:justify-start gap-6"
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             transition={{ duration: 1, delay: 1 }}
@@ -106,7 +106,7 @@ const Hero = () => {
           animate={{ opacity: 1, scale: 1 }}
           transition={{ duration: 1, delay: 1 }}
         >
-          <div className="relative w-80 h-80 md:w-[28rem] md:h-[28rem] rounded-3xl overflow-hidden border-4 border-violet-500 shadow-2xl transform hover:scale-105 transition-transform duration-500">
+          <div className="relative w-56 h-56 sm:w-64 sm:h-64 md:w-80 md:h-80 lg:w-[28rem] lg:h-[28rem] mb-20 rounded-3xl overflow-hidden border-4 border-violet-500 shadow-2xl transform hover:scale-105 transition-transform duration-500">
             <img
               src="https://cdn.pixabay.com/photo/2021/08/04/13/06/software-developer-6521720_1280.jpg"
               alt="John Wesley"
@@ -118,7 +118,7 @@ const Hero = () => {
 
       {/* Scroll Indicator */}
       <motion.div
-        className="absolute bottom-10 left-1/2 transform -translate-x-1/2 flex flex-col items-center"
+        className="absolute bottom-10 max-md:hidden left-1/2 transform -translate-x-1/2 flex flex-col items-center"
         initial={{ opacity: 0, y: 20 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 1, delay: 1.5 }}
